8262ad91f1b25a718a4ed29475a21304f010e968
Marc Lewis <marc8@live.unc.edu>
2019-09-09 16:02:36 -0400

    Render buckets

--- board-script.ts
+++ board-script.ts
@@ -1,18 +1,42 @@
 import { Application, interaction } from "pixi.js";
 
+const SCALE_X: number = 100;
+const SCALE_Y: number = 100;
+const OFFSET: number = 60;
+const CIRCLE_RADIUS: number = 30;
+
 export let drawBoard = (): void => {
 
-    const app: Application = new Application({ width: 496, height: 496, backgroundColor: 0xFFFFFF });
+    const app: Application = new Application({ width: window.innerWidth - 20, height: window.innerHeight - 20, backgroundColor: 0xFFFFFF });
     document.body.appendChild(app.view);
 
     // Initialize the pixi Graphics class
-    let graphics = new PIXI.Graphics();
-    graphics.beginFill(0x0000ff);
-    graphics.drawRoundedRect(0, 0, app.view.width, app.view.height, 30);
-    let board = graphics.endFill();
+    let board = new PIXI.Graphics();
+    board.beginFill(0x0000ff);
+    board.drawRoundedRect(0, 0, app.view.width, app.view.height, 30);
+    board.endFill();
     board.interactive = true;
     board.on("mousedown", (event: interaction.InteractionEvent) => {console.log("we clicked"); });
 
+    app.stage.addChild(board);
+    
+    for (let row = 0; row < 2; row++) {
+        for (let col = 0; col < 6; col++) {
 
-    app.stage.addChild(graphics);
+            let bucket = new PIXI.Graphics();
+            bucket.beginFill(0xff0000);
+            bucket.drawCircle(col * SCALE_X + OFFSET, row * SCALE_Y + OFFSET, CIRCLE_RADIUS);
+            bucket.endFill();
+            bucket.interactive = true;
+            bucket.name = row + ", " + col;
+            bucket.on("mousedown", (event: interaction.InteractionEvent) => {console.log("clicked on " + bucket.name); });
+            app.stage.addChild(bucket);
+
+        }
+
+
+    }
+
+
+
 };
