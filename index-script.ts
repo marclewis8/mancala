120292e724da8f482c6470594025c6386cf482e3
Marc Lewis <marc8@live.unc.edu>
2019-12-22 14:58:56 -0500

    Comments and readability improvements

--- index-script.ts
+++ index-script.ts
@@ -1,6 +1,4 @@
 import { drawBoard, drawPlayerTurn } from "./board-script";
-import { interaction } from "pixi.js";
-import { assertLVal } from "babel-types";
 
 export const numStones: number = 48;
 export const numRows: number = 2;
@@ -17,64 +15,85 @@ export let main = async () => {
 };
 
 export let initModel = (): void => {
-    for (let row = 0; row < numRows; row++) {
-        model[row] = [];
-        for (let col = 0; col < numCols; col++) {
-            model[row][col] = 4;
+    for (let row = 0; row < numRows; row++) { // run twice, once for each row
+        model[row] = []; // initialize row
+        for (let col = 0; col < numCols; col++) { // run six times, once for each column
+            model[row][col] = 4; // initialize index
         }
     }
 };
 
 export let onClick = (row: number, col: number): boolean => {
-    let stonesInHand = model[row][col];
-    model[row][col] = 0;
+    let stonesInHand = model[row][col]; // store amount of stones at row,col in variable
+    model[row][col] = 0; // clear out index
+
     if (row === 0) {
         // we're on top row, which is player 0 moving left
         if (player === 1 || stonesInHand === 0) {
-            model[row][col] = stonesInHand;
+            // player 1 cannot click on row 0
+            // player can't click on empty bucket
+            model[row][col] = stonesInHand; // put stones back and stop function
             return false;
         }
-        let direction = -1;
+        let direction = -1; // move counterclockwise, on row 0 this means go left
         let goAgain = false;
-        while (stonesInHand > 0) {
+
+        while (stonesInHand > 0) { 
+            // one cycle = maximum of one stone placed
+            // row, col now used to track what index we're "hovering over", not the start index
             col += direction;
-            if (col === -1) {
+
+            if (col === -1) { 
+                // case where player 0's store is reached
                 p0Score++;
                 row++;
-                direction *= -1;
+                direction *= -1; // start moving right on row 1, equiv to direction = 1;
                 if (stonesInHand === 1) {
-                    // Get to go again
+                    // get to go again
                     goAgain = true;
                 }
                 stonesInHand--;
-            } else if (col === numCols) {
+
+            } else if (col === numCols) { 
+                // case where player 1's store is reached on player 0's turn
                 row--;
-                direction *= -1;
-            } else if (model[row][col] === 0 && stonesInHand === 1 && row === 0) {
+                direction *= -1; // start moving left on row 0, equiv to direction = 1
+
+            } else if (model[row][col] === 0 && stonesInHand === 1 && row === 0) { 
+                // case to steal stones from index across from bucket
                 p0Score += 1 + model[1][col];
                 model[1][col] = 0;
                 stonesInHand--;
+
             } else {
                 model[row][col]++;
                 stonesInHand--;
+
             }
         }
+
+        // end loop, change player variable only if goAgain is false
         if (!goAgain) {
             player = 1;
         }
+
     } else {
         // we're on the bottom, which is player 1 moving right
 
         if (player === 0 || stonesInHand === 0) {
-            model[row][col] = stonesInHand;
+            // player 0 cannot click on row 1
+            // player cannot click on empty bucket
+            model[row][col] = stonesInHand; // put stones back and stop function
             return false;
         }
+
         let goAgain = false;
-        let direction = 1;
+        let direction = 1; // on row 1, start moving right
         while (stonesInHand > 0) {
             col += direction;
+
             if (col === numCols) {
-                // We're at our bank
+                // At player 1's store, on player 1's turn (deposit)
                 p1Score++;
                 row--;
                 direction *= -1;
@@ -82,22 +101,31 @@ export let onClick = (row: number, col: number): boolean => {
                     goAgain = true;
                 }
                 stonesInHand--;
+
             } else if (col === -1) {
+                // At player 0's store on player 1's turn (skip)
                 row++;
                 direction = 1;
+
             } else if (model[row][col] === 0 && stonesInHand === 1 && row === 1) {
+                // case to steal stones across, now stealing from row 0
                 p1Score += 1 + model[0][col];
                 model[0][col] = 0;
                 stonesInHand--;
+
             } else {
                 model[row][col]++;
                 stonesInHand--;
+
             }
         }
+
         if (!goAgain) {
             player = 0;
         }
+
     }
+    // run function after each turn
     checkIfGameOver();
     return true;
 };
@@ -106,28 +134,32 @@ export let onClick = (row: number, col: number): boolean => {
 export let checkIfGameOver = () => {
     let sum1 = sumRow(1);
     let sum0 = sumRow(0);
+
+    // game ends when sum0 or sum1 is true
     if (sum0 === 0 || sum1 === 0) {
-        clearRow(0);
+        clearRow(0); // clear out both rows in model, sum values stored already
         clearRow(1);
-        p0Score += sum0;
+        p0Score += sum0; // one of these will simply add 0 to the player's score (b/c of condition), which is fine
         p1Score += sum1;
         if (p1Score > p0Score) {
             winner = 1;
         } else if (p0Score > p1Score) {
             winner = 0;
         } else {
-            winner = -1;
+            winner = -1; // -1 = tie
         }
     }
 };
 
 export let clearRow = (row: number) => {
+    // helper function to clear out each row
     for (let col = 0; col < model[row].length; col++) {
         model[row][col] = 0;
     }
 };
 
 export let sumRow = (row: number) => {
+    // helper function to sum each row
     let count = 0;
     for (let col = 0; col < numCols; col++) {
         count += model[row][col];
@@ -135,16 +167,16 @@ export let sumRow = (row: number) => {
     return count;
 };
 
-export let handleGameOver = (sum0: number, sum1: number) => {
-    p0Score += sum0;
-    p1Score += sum1;
-    if (p1Score > p0Score) {
-        winner = 1;
-    } else if (p0Score > p1Score) {
-        winner = 0;
-    } else {
-        winner = -1;
-    }
-};
+// export let handleGameOver = (sum0: number, sum1: number) => {
+//     p0Score += sum0;
+//     p1Score += sum1;
+//     if (p1Score > p0Score) {
+//         winner = 1;
+//     } else if (p0Score > p1Score) {
+//         winner = 0;
+//     } else {
+//         winner = -1;
+//     }
+// }; dead code, remove this
 
 main();
