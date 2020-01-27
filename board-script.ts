ee32324538efecb5a9933eed5353efc52c6e0f69
JC Weber <jcweber10@gmail.com>
2020-01-27 16:01:54 -0500

    Begin graphic overhaul

--- board-script.ts
+++ board-script.ts
@@ -1,11 +1,13 @@
 import { Application, interaction } from "pixi.js";
 import { model, onClick, p1Score, p0Score, winner, player } from "./index-script";
 
-const SCALE_X: number = 100;
-const SCALE_Y: number = 100;
+const SCALE_X: number = window.innerWidth / 13.3;
 const OFFSET: number = 60;
-const CIRCLE_RADIUS: number = 30;
-let boardX = window.innerWidth / 2 + 100;
+const BUCKET_X_OFFSET = window.innerWidth / 4.25;
+const ROW_0_Y = window.innerHeight / 3.2;
+const ROW_1_Y = window.innerHeight / 1.28;
+const CIRCLE_RADIUS: number = window.innerWidth / 45;
+const STORE_X_OFFSET = window.innerWidth / 4;
 const NUM_BUCKETS: number = 12;
 let currentPlayer: number = 0;
 let buckets: PIXI.Graphics[][] = [];
@@ -13,7 +15,10 @@ let playerTurnText: PIXI.Text = new PIXI.Text(`Player ${currentPlayer}`);
 const app: Application = new Application({ width: window.innerWidth - 20, height: window.innerHeight - 20, backgroundColor: 0xFFFFFF });
 
 let p1store = new PIXI.Graphics();
-let p2store = new PIXI.Graphics();
+let p0store = new PIXI.Graphics();
+// TODO: Change image for stones
+// Make buckets and stores a little bigger/ make sure we're happy with alignment
+// Reposition player scores
 
 export let drawBoard = (): void => {
     document.body.appendChild(app.view);
@@ -25,15 +30,7 @@ export let drawBoard = (): void => {
 };
 
 let drawBackground = () => {
-    let board = new PIXI.Graphics();
-    board.beginFill(0x0000ff);
-    board.drawRoundedRect(0, 0, app.view.width, app.view.height, 30);
-    board.endFill();
-    // board.interactive = true;
-    // board.on("mousedown", (event: interaction.InteractionEvent) => { console.log("we clicked"); });
-    app.stage.addChild(board);
-
-    // let board = PIXI.Sprite.from("board.png"); DO NOT UNCOMMENT THIS PLS
+    let board = PIXI.Sprite.from("board.png");
     board.width = window.innerWidth;
     board.height = window.innerHeight;
     app.stage.addChild(board);
@@ -45,32 +42,32 @@ let drawBuckets = () => {
         for (let col = 1; col < 7; col++) {
             let bucket = new PIXI.Graphics();
             bucket.beginFill(row === 0 ? 0xffff00 : 0x00ff00);
-            bucket.drawCircle(col * SCALE_X + OFFSET, row * SCALE_Y + OFFSET, CIRCLE_RADIUS);
+            bucket.drawCircle(col * SCALE_X + BUCKET_X_OFFSET, getRowY(row), CIRCLE_RADIUS);
             bucket.endFill();
             bucket.interactive = true;
             bucket.name = row + ", " + (col - 1);
             bucket.on("mousedown", ourOnClick);
             buckets[row][col - 1] = bucket;
             app.stage.addChild(bucket);
-
-            let amountOfStones: PIXI.Text = new PIXI.Text("");
-            // bucket.add              
         }
     }
 };
 
 
 let initStores = () => {
+    let storeWidth = CIRCLE_RADIUS * 2;
+    let storeHeight = getRowY(1) - getRowY(0) + storeWidth;
+    console.log(window.innerWidth);
+    p0store.beginFill(0xffff00);
+    p0store.drawRoundedRect(STORE_X_OFFSET - CIRCLE_RADIUS * 2, getRowY(0) - CIRCLE_RADIUS, storeWidth, storeHeight, 30);
+    p0store.endFill();
 
     p1store.beginFill(0x00ff00);
-    p1store.drawRoundedRect(boardX, OFFSET - CIRCLE_RADIUS, CIRCLE_RADIUS * 2, CIRCLE_RADIUS * 3 + OFFSET, 30);
+    p1store.drawRoundedRect(window.innerWidth - STORE_X_OFFSET, getRowY(0) - CIRCLE_RADIUS, storeWidth, storeHeight, 30);
     p1store.endFill();
-    app.stage.addChild(p1store);
 
-    p2store.beginFill(0xffff00);
-    p2store.drawRoundedRect(OFFSET, OFFSET - CIRCLE_RADIUS, CIRCLE_RADIUS * 2, CIRCLE_RADIUS * 3 + OFFSET, 30);
-    p2store.endFill();
-    app.stage.addChild(p2store);
+    app.stage.addChild(p0store);
+    app.stage.addChild(p1store);
 };
 
 let drawStores = () => {
@@ -88,22 +85,22 @@ let drawStores = () => {
 
     let p1amountOfStones: PIXI.Text = new PIXI.Text("" + p1Score);
     p1store.addChild(p1amountOfStones);
-    p1amountOfStones.position.x = boardX;
+    p1amountOfStones.position.x = STORE_X_OFFSET;
     p1amountOfStones.position.y = OFFSET - CIRCLE_RADIUS + 150;
 
 
-    p2store.removeChildren();
+    p0store.removeChildren();
     for (let numStones = 0; numStones < p0Score; numStones++) {
-        let bounds = p2store.getBounds();
+        let bounds = p0store.getBounds();
         let stone = PIXI.Sprite.from("rameses.png");
         stone.scale = new PIXI.Point(.05, .05);
         stone.anchor.set(.5);
         stone.x = getRandomInt(bounds.left + 20, bounds.right - 20);
         stone.y = getRandomInt(bounds.top + 20, bounds.bottom - 20);
-        p2store.addChild(stone);
+        p0store.addChild(stone);
     }
     let p2amountOfStones: PIXI.Text = new PIXI.Text("" + p0Score);
-    p2store.addChild(p2amountOfStones);
+    p0store.addChild(p2amountOfStones);
     p2amountOfStones.position.x = OFFSET;
     p2amountOfStones.position.y = OFFSET - CIRCLE_RADIUS + 150;
 };
@@ -128,10 +125,10 @@ let drawStones = (): void => {
                 bucket.addChild(stone);
             }
             let amountOfStones = new PIXI.Text("" + model[row][col]);
+            amountOfStones.style.fill = 0xFFFFFF;
             bucket.addChild(amountOfStones);
-            amountOfStones.position.x = (col + 1) * SCALE_X + OFFSET - 10;
-            amountOfStones.position.y = row * SCALE_Y + OFFSET + 30;
-
+            amountOfStones.position.x = (col + 1) * SCALE_X + BUCKET_X_OFFSET;
+            amountOfStones.position.y = getNumStonesLabelY(row);
         }
     }
 };
@@ -149,7 +146,6 @@ let isInBounds = (x: number, y: number, rect: PIXI.Rectangle): boolean => {
 };
 
 export let getIndexFromClick = (event: interaction.InteractionEvent): string => {
-
     let x = event.data.global.x;
     let y = event.data.global.y;
     for (let row = 0; row < buckets.length; row++) {
@@ -189,4 +185,10 @@ let ourOnClick = (event: interaction.InteractionEvent) => {
     } else {
         alert("Not your turn or your bucket is empty");
     }
+};
+
+let getRowY = (row: number) => row === 0 ? ROW_0_Y : ROW_1_Y;
+let getNumStonesLabelY = (row: number) => {
+    let offset = window.innerHeight / 8;
+    return row === 0 ? ROW_0_Y - offset : ROW_1_Y + offset / 1.6;
 };
